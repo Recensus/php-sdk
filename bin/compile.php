14e570c64fd264e96c211cde5e924c6f7cba0841
Ben Waine <ben@reensus.comwq>
2012-12-19 12:29:53 -0300

    Fixed the compiler script.
    * Now uses new class names
    * Provides the option to compile with or without ZendFramework classes.
    (Added becuase Magento already includes the ZF classes and requiring
    them again causes issues).

--- bin/compile.php
+++ bin/compile.php
@@ -14,46 +14,55 @@ require_once __DIR__ . '/../vendor/autoload.php';
 @mkdir(__DIR__ . '/../build', 0777, true);
 @unlink(__DIR__ . '/../build/recensus.php');
 
+$withDeps = (getenv('COMPILE_DEPS'));
 
-$classes = array(
-    'Zend_Exception',
-    'Zend_Registry',
-    'Zend_Uri',
-    'Zend_Uri_Exception',
-    'Zend_Http_Client',
-    'Zend_Http_Exception',
-    'Zend_Http_Response',
-    'Zend_Http_Client_Exception',
-    'Zend_Http_Client_Adapter_Curl',
-    'Zend_Http_Client_Adapter_Exception',
-    'Zend_Http_Client_Adapter_Interface',
-    'Zend_Http_Client_Adapter_Proxy',
-    'Zend_Http_Client_Adapter_Socket',
-    'Zend_Http_Client_Adapter_Stream',
-    'Zend_Http_Client_Adapter_Test',
-    'Zend_Http_Response_Stream',
-    'Zend_Uri_Http',
-    'Zend_Validate_Abstract',
-    'Zend_Validate_Exception',
-    'Zend_Validate_Hostname',
-    'Zend_Validate_Interface',
-    'Zend_Validate_Ip',
-    'Zend_Loader',
-    'Zend_Loader_Autoloader',
-    'Zend_Validate_Hostname_Biz',
-    'Zend_Validate_Hostname_Cn',
-    'Zend_Validate_Hostname_Com',
-    'Zend_Validate_Hostname_Jp',
-    "RecensusApi",
-    "RecensusApiException",
-    "RecensusWidget",
-    "RecensusWidgetException",
-    
-);
+if ($withDeps) {
+
+    $classes = array(
+        'Zend_Exception',
+        'Zend_Registry',
+        'Zend_Uri',
+        'Zend_Uri_Exception',
+        'Zend_Http_Client',
+        'Zend_Http_Exception',
+        'Zend_Http_Response',
+        'Zend_Http_Client_Exception',
+        'Zend_Http_Client_Adapter_Curl',
+        'Zend_Http_Client_Adapter_Exception',
+        'Zend_Http_Client_Adapter_Interface',
+        'Zend_Http_Client_Adapter_Proxy',
+        'Zend_Http_Client_Adapter_Socket',
+        'Zend_Http_Client_Adapter_Stream',
+        'Zend_Http_Client_Adapter_Test',
+        'Zend_Http_Response_Stream',
+        'Zend_Uri_Http',
+        'Zend_Validate_Abstract',
+        'Zend_Validate_Exception',
+        'Zend_Validate_Hostname',
+        'Zend_Validate_Interface',
+        'Zend_Validate_Ip',
+        'Zend_Loader',
+        'Zend_Loader_Autoloader',
+        'Zend_Validate_Hostname_Biz',
+        'Zend_Validate_Hostname_Cn',
+        'Zend_Validate_Hostname_Com',
+        'Zend_Validate_Hostname_Jp');
+} else {
+    $classes = array();
+}
+
+$classesToCompile = array_merge($classes, array(
+    "Recensus_Api",
+    "Recensus_Api_Exception",
+    "Recensus_Widget",
+    "Recensus_Widget_Exception",
+        ));
+
+
 
 $ccl = new ClassCollectionLoader();
 
-$ccl->load($classes, __DIR__ . '/../build', 'recensus', false);
+$ccl->load($classesToCompile, __DIR__ . '/../build', 'recensus', false);
 
 $debug = getenv('COMPILE_DEBUG');
 
